b0857b134dec12d1e36750d42f9590f062e926a6
Predator <alexderugo@gmail.com>
2025-06-29 17:39:49 +0400

    first commit

---
+++ src/rainbowKitConfig.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { anvil, zksync } from "wagmi/chains";
+
+export default getDefaultConfig({
+    appName: "TSender",
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+    chains: [anvil, zksync],
+    ssr: false,
+
+})
